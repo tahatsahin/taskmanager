edd8556dbab7c3845abf2bf87d1e619b0df04cad
Taha <tahatsahin@gmail.com>
2023-03-23 18:04:01 +0300

    add test for taskController

--- controllers/taskController_test.go
+++ controllers/taskController_test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -9,25 +10,19 @@ import (
 	"testing"
 )
 
-type Data struct {
-	CreatedBy   string `json:"createdBy"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-}
-
-type Task struct {
-	Data Data `json:"data"`
-}
-
 func TestCreateTask(t *testing.T) {
 	common.StartUp()
 
-	file, err := os.Open("data.json")
+	file, err := os.Open("testData.json")
 	if err != nil {
 		log.Fatalf("cannot open file %v", err)
 	}
-	defer file.Close()
-
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatalf("cannot close file")
+		}
+	}(file)
 	req, err := http.NewRequest(http.MethodPost, "/tasks", file)
 	if err != nil {
 		log.Fatalf("cannot create request: %v", err)
@@ -62,23 +57,73 @@ func TestGetTasks(t *testing.T) {
 func TestUpdateTask(t *testing.T) {
 	common.StartUp()
 
-	file, err := os.Open("data.json")
+	file, err := os.Open("testData.json")
 	if err != nil {
 		log.Fatalf("cannot open file %v", err)
 	}
-	defer file.Close()
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatalf("cannot close file")
+		}
+	}(file)
 
-	req, err := http.NewRequest(http.MethodPut, "/tasks/63fddf39a5643c560f030d14", file)
+	req, err := http.NewRequest(http.MethodPut, "/tasks/", file)
 	if err != nil {
 		log.Fatalf("cannot create request: %v", err)
 	}
 
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(UpdateTask)
+	var endpointVar = map[string]string{
+		"id": "63111153a5643c560f030d15",
+	}
+	req = mux.SetURLVars(req, endpointVar)
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusCreated {
 		t.Errorf("handler returned wrong status code: got %v, want %v", status, http.StatusCreated)
 	}
-	log.Println(rr.Body)
+}
+
+func TestGetTaskById(t *testing.T) {
+	common.StartUp()
+
+	req, err := http.NewRequest(http.MethodGet, "/tasks/", nil)
+	if err != nil {
+		log.Fatalf("cannot create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetTaskById)
+	endpointVar := map[string]string{
+		"id": "63111153a5643c560f030d15",
+	}
+	req = mux.SetURLVars(req, endpointVar)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v, want %v", status, http.StatusOK)
+	}
+}
+
+func TestDeleteTask(t *testing.T) {
+	common.StartUp()
+
+	req, err := http.NewRequest(http.MethodDelete, "/tasks/", nil)
+	if err != nil {
+		log.Fatalf("cannot create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(DeleteTask)
+	endpointVar := map[string]string{
+		"id": "63111153a5643c560f030d15",
+	}
+	req = mux.SetURLVars(req, endpointVar)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusNoContent {
+		t.Errorf("handler returned wrong status code: got %v, want %v", status, http.StatusOK)
+	}
 }
